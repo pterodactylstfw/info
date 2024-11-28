15140b2ff4b65c51eb0450c3064d712c40e083d9
pterodactylstfw <constantin.raul10@gmail.com>
2024-11-28 21:49:00 +0200

    a

--- Facultate/AF/lucru_acasa/test/test/main.cpp
+++ Facultate/AF/lucru_acasa/test/test/main.cpp
@@ -3,44 +3,56 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <ctime>
+
+//std::ifstream fin("date.txt");
+//
+//std::vector<int> citire()
+//{
+//    std::vector<int> vect;
+//    int n;
+//    
+//    std::cin>>n;
+//    
+//    vect.resize(n);
+//    
+//    for(int i=0;i<vect.size();i++)
+//        std::cin>>vect[i];
+//    
+//    return vect;
+//}
+//
+//void afisare(std::vector<int> vect)
+//{
+//    for(const auto& elem: vect)
+//        std::cout<<elem<<" ";
+//    std::cout<<'\n';
+//}
+//
+//std::vector<std::vector<int>> citire()
+//{
+//    std::vector<std::vector<int>> matr;
+//    int n, m;
+//    std::cin>>n>>m;
+//    
+//    matr.resize(n);
+//    for(int i=0;i<n;i++) matr[i].resize(m);
+//    
+//    for(int i=0;i<n;i++)
+//        for(int j=0;j<m;j++)
+//            std::cin>>matr[i][j];
+//    
+//    return matr;
+//}
 
 
-std::ifstream fin("date.txt");
-
-std::vector<int> citire()
+int main()
 {
-    std::vector<int> vect;
-    int n;
-    
-    std::cin>>n;
-    
-    vect.resize(n);
-    
-    for(int i=0;i<vect.size();i++)
-        std::cin>>vect[i];
-    
-    return vect;
-}
+    srand(time(0));
 
-void afisare(std::vector<int> vect)
-{
-    for(const auto& elem: vect)
-        std::cout<<elem<<" ";
-    std::cout<<'\n';
-}
+    // Generate a random number between 0 and 100
+    int randomNum = rand() % 101;
 
-std::vector<std::vector<int>> citire()
-{
-    std::vector<std::vector<int>> matr;
-    int n, m;
-    std::cin>>n>>m;
-    
-    matr.resize(n);
-    for(int i=0;i<n;i++) matr[i].resize(m);
-    
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-            std::cin>>matr[i][j];
-    
-    return matr;
+    std::cout << randomNum;
+    return 0;
 }
