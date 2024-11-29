a7935575264c648a82144698bcc12e4b33924e18
Raul-Nicolae Constantin <constantin.raul10@gmail.com>
2024-11-29 08:51:33 +0200

    final_Facultate_pregatire

--- Facultate/AF/lucru_acasa/lucru_supl_partial/lucru_supl_partial/problema1.cpp
+++ Facultate/AF/lucru_acasa/lucru_supl_partial/lucru_supl_partial/problema1.cpp
@@ -1,22 +1,53 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 
-std::vector<std::vector<int>> citire_matrice()
-{
-    int n, m;
-    fin>>n>>m;
-    std::vector<std::vector<int>> mat;
+std::vector<int> citire() {
+    int dim;
+    std::cin >> dim;
+    std::vector<int> v(dim);
+    for (int i = 0; i < dim; i++) {
+        std::cin >> v[i];
+    }
+    return v;
+}
 
+void afisare(std::vector<int> v) {
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << '\n';
+}
 
-    mat.resize(n);
+bool comparatorCrescator(int x, int y) {
+    return x < y;
+}
 
-    for(int i = 0 ;i< n;i ++)
-        mat[i].resize(m);
+bool comparatorDescrescator(int x, int y) {
+    return x > y;
+}
 
+void quicksort(std::vector<int>& v, int st, int dr, bool comparator(int, int)) {
+    if (st < dr) {
+        int piv = v[(st + dr) / 2];
+        int i = st, j = dr;
+        while (i <= j) {
+            while (comparator(v[i], piv))i++;
+            while (comparator(piv, v[j]))j--;
+            if (i <= j) {
+                int aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+                i++;
+                j--;
+            }
+        }
+        quicksort(v, st, j, comparator);
+        quicksort(v, i, dr, comparator);
+    }
+}
 
-    for(int i = 0; i<n;i++)
-        for(int j = 0;j<m;j++)
-        fin>>mat[i][j];
-
-        return mat;
+int main() {
+    std::vector<int> v = citire();
+    quicksort(v, 0, v.size() - 1, comparatorCrescator);
+    afisare(v);
 }
